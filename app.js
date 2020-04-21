0be5b4c18553d00d62a17913568128f684604b1d
contrastereo <lpirela90@gmail.com>
2020-04-20 21:33:27 -0300

    Basic functionality completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body')
 const board = document.querySelector(".board")
-
+const reset = document.querySelector(".reset")
+const more = document.querySelector(".more")
 let numOfSquares = 16;
 const sqmaker = ()=>{
   let sqTotal = (numOfSquares*numOfSquares);
@@ -20,3 +21,28 @@ const sqmaker = ()=>{
 
 
 document.addEventListener('DOMContentLoaded', sqmaker);
+
+reset.addEventListener('click', ()=> {
+  arr = Array.from(board.querySelectorAll(".square"));
+  arr.forEach(e =>{
+    e.style.backgroundColor = "white"
+  })
+})
+
+more.addEventListener('click', ()=> {
+  board.innerHTML = ""
+  numOfSquares = Number(prompt('Tell me a number'))
+  let sqTotal = (numOfSquares*numOfSquares);
+  let sqSize = 100/numOfSquares
+  document.querySelector(".board").style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`
+  document.querySelector(".board").style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`
+  for(i = 0; i < sqTotal; i++){
+    let sq = document.createElement('div')
+    sq.className = 'square';
+    sq.addEventListener("mouseover", () =>{
+      sq.style.backgroundColor="black"
+    })
+    board.appendChild(sq)
+  }
+  
+})
